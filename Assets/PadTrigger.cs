13ee77a18aad2bb67072d7007fa80b71a5a079a2
sksmsaidkeu <happyp409man@gmail.com>
2026-05-30 16:12:27 +0900

    chore: 주석 추가

--- Assets/PadTrigger.cs
+++ Assets/PadTrigger.cs
@@ -13,26 +13,26 @@ public class PadTrigger : MonoBehaviour
     public float padPressDepth = 0.1f;
     public float padSpeed = 5f;
 
-    private Vector3 startPosition;
-    private Vector3 targetPosition;
-    private Vector3 padStartPosition;
-    private Vector3 padPressedPosition;
-    private bool isPressed = false;
-    private doorPhysics doorBlockCheck;
+    private Vector3 startPosition;       //door 시작점
+    private Vector3 targetPosition;       //door 끝점
+    private Vector3 padStartPosition;      //pad 시작점
+    private Vector3 padPressedPosition;     //pad 상호작용시 내려갈 정도
+    private bool isPressed = false;          // door가 player 오브젝트와 충돌했는지의 여부(얘는 door의 두번째 rigidbody를 이용해서 설정)
+    private doorPhysics doorBlockCheck;       // 위에랑 같이 쓰는거
 
     void Start()
     {
-        if (doorRigidbody == null)
+        if (doorRigidbody == null)     //door에 rigidbody가 할당되지 않으면 작동x
         {
             Debug.LogError("doorRigidbody가 할당되지 않았습니다.");
             return;
         }
 
         doorRigidbody.isKinematic = true;
-        doorRigidbody.useGravity = false;
+        doorRigidbody.useGravity = false;   // door 오브젝트는 중력의 영향을 받지 않게 설정
 
-        startPosition = doorRigidbody.position;
-        targetPosition = startPosition + new Vector3(0, targetYOffset, 0);
+        startPosition = doorRigidbody.position;  //door의 rigidbody가 시작점
+        targetPosition = startPosition + new Vector3(0, targetYOffset, 0);  //door가 y축으로 어느정도까지 움직일 지 설정
 
         padStartPosition = transform.position;
         padPressedPosition = padStartPosition - new Vector3(0, padPressDepth, 0);
--- Assets/doorPhysics.cs
+++ Assets/doorPhysics.cs
@@ -15,3 +15,5 @@ public class doorPhysics : MonoBehaviour
             isBlocked = false;
     }
 }
+
+//doorPhysics에서는 door 오브젝트가 player 오브젝트와 충돌했는가 아닌가의 여부만 확인, 세부적인 움직임은 padTrigger.cs에서 동작
